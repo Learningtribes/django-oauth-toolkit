25c27f885f2a5498bed8d3e085a3bf728fcbe00f
Aaron Zhao <rudy710@qq.com>
2022-02-21 12:40:52 +0800

    debug admin

--- oauth2_provider/admin.py
+++ oauth2_provider/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Grant, AccessToken, RefreshToken, get_application_model
+from .models import Grant, AccessToken, RefreshToken, get_application_model, Application
 
 
+@admin.register(Application)
 class ApplicationAdmin(admin.ModelAdmin):
     list_display = ("name", "user", "client_type", "authorization_grant_type")
     list_filter = ("client_type", "authorization_grant_type", "skip_authorization")
@@ -33,9 +34,9 @@ class RefreshTokenAdmin(admin.ModelAdmin):
     raw_id_fields = ("user", "access_token")
 
 
-Application = get_application_model()
+# Application = get_application_model()
 
-admin.site.register(Application, ApplicationAdmin)
+# admin.site.register(Application, ApplicationAdmin)
 admin.site.register(Grant, GrantAdmin)
 admin.site.register(AccessToken, AccessTokenAdmin)
 admin.site.register(RefreshToken, RefreshTokenAdmin)
