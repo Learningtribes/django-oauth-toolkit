038603df4d8a9b040c662a31359ffafebec362a9
Cristiano de Paula <crdepa@gmail.com>
2024-01-02 20:27:42 +0000

    fixing validator rule

--- oauth2_provider/oauth2_validators.py
+++ oauth2_provider/oauth2_validators.py
@@ -126,7 +126,7 @@ class OAuth2Validator(RequestValidator):
         if self._load_application(client_id, request) is None:
             log.debug("Failed body auth: Application %s does not exists" % client_id)
             return False
-        elif request.client.client_secret != client_secret:
+        elif not self._check_secret(client_secret, request.client.client_secret):
             log.debug("Failed body auth: wrong client secret %s" % client_secret)
             return False
         else:
